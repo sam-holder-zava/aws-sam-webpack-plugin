1870f1f943eecdf7146d9783a39c02d5326ff52e
Samuel Holder <samuelholder@ip-192-168-1-9.eu-west-1.compute.internal>
2022-01-18 09:59:33 +0000

    added multiple template sample.

---
+++ samples/multiple-templates/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const AwsSamPlugin = require('aws-sam-webpack-plugin')
+const awsSamPlugin = new AwsSamPlugin({
+  projects: {
+    projectA: "./appA",
+    projectB: "./appB"
+  }
+})
+
+module.exports = {
+  entry: () => awsSamPlugin.entry(),
+  
+  output: {
+    filename: (chunkData) => awsSamPlugin.filename(chunkData),
+    libraryTarget: 'commonjs2',
+    path: path.resolve('.')
+  },
+
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  
+  target: 'node',
+  externals: process.env.NODE_ENV === 'development' ? [] : ['aws-sdk'],
+  mode: process.env.NODE_ENV || 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(ts)$/,
+        loader: 'ts-loader',
+        include: [
+          path.resolve(__dirname, 'appA', 'src'),
+          path.resolve(__dirname, 'appB', 'src'),
+        ],
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.webpack'),
+          ],
+        ],
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+        },
+      },
+    ]
+  },
+
+  plugins: [
+    awsSamPlugin
+  ],
+
+}
